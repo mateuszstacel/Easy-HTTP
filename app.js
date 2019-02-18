caace2a8f7d421abada6676b1e16824ac2c0b949
mateusz stacel <mateuszstacel@mateuszs-MacBook-Pro.local>
2019-02-18 13:32:21 +0000

    Post method

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const http = new easyHTTP;
 
-//get post
+//get posts
 
-const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
+// const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//     if(err){
+//         console.log(err);
+//     } else {
+//          console.log(posts);
+//     }
+// });
+
+//get single post
+
+// const posts = http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post){
+//     if(err){
+//         console.log(err);
+//     } else {
+//          console.log(post);
+//     }
+// });
+
+//create data
+
+const data = {
+    title: 'custom post',
+    body: 'custom post'
+};
+// Post post
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
     if(err){
         console.log(err);
     } else {
-         console.log(response);
+         console.log(post);
     }
 });
+
+
+
 
--- easyhttp.js
+++ easyhttp.js
@@ -21,6 +21,20 @@ function easyHTTP() {
 
 // post
 
+easyHTTP.prototype.post = function (url, data, callback) {
+    this.http.open('post', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json')
+
+    let self = this;
+
+    this.http.onload = function() {
+    
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
 // put
 
 // delete
